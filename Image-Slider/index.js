418d304fe39a9bb22d0ff97eaa0d182c89a31e51
kevin stratvert <melalamuleta54@gmail.com>
2026-03-27 17:28:31 +0300

    Add responsive image slider with navigation buttons and auto-slide

---
+++ Image-Slider/index.js
@@ -0,0 +1,19 @@
+const slides=document.querySelectorAll(".slide"); 
+let slideindex=0;
+let intervalID=null;
+document.addEventListener("DOMContentLoaded",intializeslider); 
+function intializeslider(){ 
+    if(slides.length>0){ 
+        slides[slideindex].classList.add("displayslide"); 
+        intervalID=setInterval(nextslide,3000)}}
+function showslide(index){ 
+    if(index>=slides.length){ slideindex=0; } 
+    else if(index<0){ slideindex=slides.length -1; } 
+    slides.forEach(slide=>{ slide.classList.remove("displayslide"); });
+    slides[slideindex].classList.add("displayslide"); } 
+function prevslide(){
+     slideindex--; 
+     showslide(slideindex); }
+function nextslide(){
+     slideindex++; 
+     showslide(slideindex); }
